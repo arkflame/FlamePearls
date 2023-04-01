0604fc4cb0adb0b5c5ebb49924894c054023765b
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2023-04-01 15:40:42 -0300

    separate findLocation helper into utils

--- src/main/java/com/arkflame/flamepearls/FlamePearls.java
+++ src/main/java/com/arkflame/flamepearls/FlamePearls.java
@@ -26,6 +26,8 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.projectiles.ProjectileSource;
 
+import com.arkflame.flamepearls.utils.LocationUtil;
+
 public class FlamePearls extends JavaPlugin implements Listener {
 
     @Override
@@ -166,7 +168,7 @@ public class FlamePearls extends JavaPlugin implements Listener {
             // Get the world of the location
             World world = location.getWorld();
             // Try to find the nearest safest position
-            Location safeLocation = findSafeLocation(location, origin != null ? origin : location, world);
+            Location safeLocation = LocationUtil.findSafeLocation(location, origin != null ? origin : location, world);
             // Teleport the player to that location
             player.teleport(safeLocation.setDirection(player.getLocation().getDirection()));
             // Damage the player
@@ -174,58 +176,5 @@ public class FlamePearls extends JavaPlugin implements Listener {
             // Play sound
             world.playSound(safeLocation, Sound.ENDERMAN_TELEPORT, 5, 1f);
         }
-    }
-
-    // A helper method that finds the nearest safest location from a given location,
-    // origin and world
-    private Location findSafeLocation(Location location, Location origin, World world) {
-        // Clone the original location
-        Location clone = location.clone();
-
-        // Check if location is already safe
-        if (clone.getBlock().getType() == Material.AIR && clone.add(0, 1, 0).getBlock().getType() == Material.AIR) {
-            // Return the fixed location
-            return location.getBlock().getLocation().add(0.5, 0, 0.5);
-        }
-
-        // Get the coordinates of the location
-        int x = location.getBlockX();
-        int y = location.getBlockY();
-        int z = location.getBlockZ();
-        // Initialize a variable to store the minimum distance to a safe block
-        double minDistance = Double.MAX_VALUE;
-        // Initialize a variable to store the best safe location
-        Location bestLocation = null;
-        // Loop through a 3x3 square around the location on the same y level
-        for (int dx = -1; dx <= 1; dx++) {
-            for (int dz = -1; dz <= 1; dz++) {
-                // Get the block at the offset coordinates
-                Material block = world.getBlockAt(x + dx, y, z + dz).getType();
-                // Check if the block is air or water
-                if (!block.isSolid()) {
-                    // Create a new location with the offset coordinates and the same pitch and yaw
-                    // as the original location
-                    Location newLocation = new Location(world, x + dx + 0.5, y, z + dz + 0.5, location.getYaw(),
-                            location.getPitch());
-                    // Calculate the distance between the new location, the original and the origin
-                    double distance = newLocation.distance(location) + newLocation.distance(origin);
-                    // Check if the distance is smaller than or equal to the minimum distance
-                    if (distance <= minDistance) {
-                        // Update the minimum distance and the best location
-                        minDistance = distance;
-                        bestLocation = newLocation;
-                    }
-                }
-            }
-        }
-        // If a safe location is found, return it
-        if (bestLocation != null) {
-            // Floor the Y value of the best location
-            bestLocation.setY(Math.floor(bestLocation.getY()));
-
-            return bestLocation;
-        }
-        // If no safe location is found, return the original location
-        return location;
     }
 }
---
+++ src/main/java/com/arkflame/flamepearls/utils/LocationUtil.java
@@ -0,0 +1,60 @@
+package com.arkflame.flamepearls.utils;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+
+public class LocationUtil {
+    // A helper method that finds the nearest safest location from a given location,
+    // origin and world
+    public static Location findSafeLocation(Location location, Location origin, World world) {
+        // Clone the original location
+        Location clone = location.clone();
+
+        // Check if location is already safe
+        if (clone.getBlock().getType() == Material.AIR && clone.add(0, 1, 0).getBlock().getType() == Material.AIR) {
+            // Return the fixed location
+            return location.getBlock().getLocation().add(0.5, 0, 0.5);
+        }
+
+        // Get the coordinates of the location
+        int x = location.getBlockX();
+        int y = location.getBlockY();
+        int z = location.getBlockZ();
+        // Initialize a variable to store the minimum distance to a safe block
+        double minDistance = Double.MAX_VALUE;
+        // Initialize a variable to store the best safe location
+        Location bestLocation = null;
+        // Loop through a 3x3 square around the location on the same y level
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dz = -1; dz <= 1; dz++) {
+                // Get the block at the offset coordinates
+                Material block = world.getBlockAt(x + dx, y, z + dz).getType();
+                // Check if the block is air or water
+                if (!block.isSolid()) {
+                    // Create a new location with the offset coordinates and the same pitch and yaw
+                    // as the original location
+                    Location newLocation = new Location(world, x + dx + 0.5, y, z + dz + 0.5, location.getYaw(),
+                            location.getPitch());
+                    // Calculate the distance between the new location, the original and the origin
+                    double distance = newLocation.distance(location) + newLocation.distance(origin);
+                    // Check if the distance is smaller than or equal to the minimum distance
+                    if (distance <= minDistance) {
+                        // Update the minimum distance and the best location
+                        minDistance = distance;
+                        bestLocation = newLocation;
+                    }
+                }
+            }
+        }
+        // If a safe location is found, return it
+        if (bestLocation != null) {
+            // Floor the Y value of the best location
+            bestLocation.setY(Math.floor(bestLocation.getY()));
+
+            return bestLocation;
+        }
+        // If no safe location is found, return the original location
+        return location;
+    }
+}
